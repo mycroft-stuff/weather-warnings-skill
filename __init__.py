85278db87328967721426877a9105b81ba44f767
mycroft-stuff <jonasbrandon00@gmail.com>
2023-01-24 18:21:03 +0100

    efffs

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+import subprocess
 
 
 class WeatherWarnings(MycroftSkill):
@@ -8,6 +9,12 @@ class WeatherWarnings(MycroftSkill):
     @intent_file_handler('warnings.weather.intent')
     def handle_warnings_weather(self, message):
         self.speak_dialog('warnings.weather')
+        
+        lookup_aemet = "lynx -dump https://www.meteoblue.com/en/weather/warnings/gandesa_spain_3121642 | grep -A5000 -m1 -e 'explanations:' | grep -vE 'explanations:|^--' | grep -m1 -B 999 -- "Instructions:" | grep -vE 'Instructions:|^--$' | tail -n +2"
+        process = subprocess.Popen(lookup_aemet.split(), stdout=subprocess.PIPE)
+        output = process.communicate()
+
+        
         self.speak_dialog("Lalala this is a test")
 
 
