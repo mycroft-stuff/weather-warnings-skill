6d5ba4096c99e5f96f39664cd500f15eb75a828f
mycroft-stuff <jonasbrandon00@gmail.com>
2023-01-24 20:31:13 +0100

    dd

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ class WeatherWarnings(MycroftSkill):
         
         xy = shlex.join(['trans ', ':en \'', str(cut5), '\''])
         
-        print('XY = ')
+        print(3 * cut5)
         
         #print(command)
         
